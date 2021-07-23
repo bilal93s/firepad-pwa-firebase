c603a1587799e64aa006eeface0df76dfca75ac9
bilal <bilal.belatoui@gmail.com>
2021-07-23 17:36:32 +0200

    test deploy

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,7 @@
-/*import firebase from "firebase/app"*/
-import * as admin from "firebase-admin";
+import firebase from "firebase/app"
 import "firebase/auth"
 
-const app = admin.initializeApp({
+const app = firebase.initializeApp({
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
